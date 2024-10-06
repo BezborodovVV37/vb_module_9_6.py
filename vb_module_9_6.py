73ce4834c646e2bf5ee572ec5a42e4ecfeb68d11
BezborodovVV37 <blood371993@gmail.com>
2024-10-06 10:53:49 +0300

    Add files via upload
    Задача:
    Напишите функцию-генератор all_variants(text), которая принимает строку text и возвращает объект-генератор, при каждой итерации которого будет возвращаться подпоследовательности переданной строки.
    
    Пункты задачи:
    Напишите функцию-генератор all_variants(text).
    Опишите логику работы внутри функции all_variants.
    Вызовите функцию all_variants и выполните итерации.
    Пример результата выполнения программы:
    Пример работы функции:
    a = all_variants("abc")
    for i in a:
    print(i)
    Вывод на консоль:
    a
    b
    c
    ab
    bc
    abc
    
    Примечания:
    Для функции генератора используйте оператор yield.

---
+++ vb_module_9_6.py
@@ -0,0 +1,16 @@
+def all_variants(text):
+    len_ = len(text)
+    for y in range(len_):
+        for i in range(len_):
+            yield text[i:i + y + 1]
+        len_ = len_ - 1
+
+
+a = all_variants("abc")
+for i in a:
+    print(i)
+print()
+
+"""b = all_variants("abcde")
+for i in b:
+    print(i)"""
